3d0d1a143739c646c83e42caa94d0f8850079e9c
deviyuliana0 <depiana1007@gmail.com>
2025-10-09 06:29:44 +0700

    Create latihan4.cpp

---
+++ 103112400151_DEVI YULIANA_S1IF-12-06_MODUL 2/WEEK 2 GUIDED/latihan4.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+
+// Prototype Prosedur
+void tulis(int x);
+
+int main() {
+    int jum;
+    cout << "Jumlah Baris kata = ";
+    cin >> jum;
+    tulis(jum);
+    return 0;
+}
+
+// Badan Prosedur
+void tulis(int x) {
+    for (int i = 0; i < x; i++) {
+        cout << "Baris ke-" << i + 1 << endl;
+    }
+}
