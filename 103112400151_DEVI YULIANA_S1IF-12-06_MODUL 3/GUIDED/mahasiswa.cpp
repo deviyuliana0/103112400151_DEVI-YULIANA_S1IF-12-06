0016179b5f7137113ede08e12c67a74a13eba7ad
deviyuliana0 <depiana1007@gmail.com>
2025-10-16 07:25:04 +0700

    Create mahasiswa.cpp

---
+++ 103112400151_DEVI YULIANA_S1IF-12-06_MODUL 3/GUIDED/mahasiswa.cpp
@@ -0,0 +1,19 @@
+#include "mahasiswa.h"
+#include <iostream>
+using namespace std;
+
+
+void inputMhs(mahasiswa &m)
+{
+    cout << "input nama = ";
+    cin >> (m).nim;
+    cout << "input nilai 1 = ";
+    cin >> (m) .nilai1;
+    cout << "input nulai 2 = ";
+    cin >> (m) .nilai2;
+
+}
+float rata2(mahasiswa m)
+{
+    return float(m.nilai1+m.nilai2)/2;
+}
