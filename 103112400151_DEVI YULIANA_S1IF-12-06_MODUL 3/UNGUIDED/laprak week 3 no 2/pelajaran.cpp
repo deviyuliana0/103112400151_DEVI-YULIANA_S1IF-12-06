788ba204fe50e804bac170c496446a32191e55c0
deviyuliana0 <depiana1007@gmail.com>
2025-10-16 07:26:03 +0700

    Create pelajaran.cpp

---
+++ 103112400151_DEVI YULIANA_S1IF-12-06_MODUL 3/UNGUIDED/laprak week 3 no 2/pelajaran.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "pelajaran.h"
+using namespace std;
+
+// Fungsi pembuat (constructor)
+pelajaran create_pelajaran(string namapel, string kodepel) {
+    pelajaran p;
+    p.namaMapel = namapel;
+    p.kodeMapel = kodepel;
+    return p;
+}
+
+// Prosedur untuk menampilkan data pelajaran
+void tampil_pelajaran(pelajaran pel) {
+    cout << "nama pelajaran : " << pel.namaMapel << endl;
+    cout << "nilai : " << pel.kodeMapel << endl;
+}
